46f6316725721c4c2ec86ccfc654ca30a808ba95
Vinicius Reis <135866349+ViniciusERF@users.noreply.github.com>
2024-04-08 20:01:16 -0300

    2commit
    ProjetoBase

---
+++ ProjetoBasico/src/pacote/ClasseConta.java
@@ -0,0 +1,12 @@
+package pacote;
+
+public class ClasseConta {
+	
+	short atributoNumeroConta; // 0
+	float atributoSaldoConta; // 0.0
+	String atributoTipoConta; // null
+	
+	String atributoNomeCliente; // null
+	int atributoDocumentoCliente; // 0
+	
+}
